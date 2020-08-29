e57e16618f563292c144f99a9987289311d60b77
kdawg <mcleod.karla@gmail.com>
2020-08-29 14:30:08 -0400

    edited profile and edit profile forms

--- client/src/components/Profile/ProfileInfo.js
+++ client/src/components/Profile/ProfileInfo.js
@@ -8,52 +8,22 @@ export class ProfileInfo extends Component {
                 <Form className="form">
                     <Form.Row>
                         <Form.Group as={Col} controlId="formGridEmail">
-                            <Form.Label>Email</Form.Label>
-                            <Form.Control type="email" placeholder="Enter email" />
+                            <Form.Label>First Name</Form.Label>
                         </Form.Group>
 
                         <Form.Group as={Col} controlId="formGridPassword">
-                            <Form.Label>Password</Form.Label>
-                            <Form.Control type="password" placeholder="Password" />
+                            <Form.Label>Last Name</Form.Label>
                         </Form.Group>
                     </Form.Row>
 
                     <Form.Group controlId="formGridAddress1">
-                        <Form.Label>Address</Form.Label>
-                        <Form.Control placeholder="1234 Main St" />
+                        <Form.Label>Location</Form.Label>
                     </Form.Group>
 
-                    <Form.Group controlId="formGridAddress2">
-                        <Form.Label>Address 2</Form.Label>
-                        <Form.Control placeholder="Apartment, studio, or floor" />
+                    <Form.Group controlId="exampleForm.ControlTextarea1">
+                        <Form.Label>About me</Form.Label>
                     </Form.Group>
 
-                    <Form.Row>
-                        <Form.Group as={Col} controlId="formGridCity">
-                            <Form.Label>City</Form.Label>
-                            <Form.Control />
-                        </Form.Group>
-
-                        <Form.Group as={Col} controlId="formGridState">
-                            <Form.Label>State</Form.Label>
-                            <Form.Control as="select" defaultValue="Choose...">
-                                <option>Choose...</option>
-                                <option>...</option>
-                            </Form.Control>
-                        </Form.Group>
-
-                        <Form.Group as={Col} controlId="formGridZip">
-                            <Form.Label>Zip</Form.Label>
-                            <Form.Control />
-                        </Form.Group>
-                    </Form.Row>
-
-                    <Form.Group id="formGridCheckbox">
-                        <Form.Check type="checkbox" label="Check me out" />
-                    </Form.Group>
-
-                    <Button variant="primary" type="submit" variant="dark">
-                        Submit</Button>
                 </Form>
             </div>
         )
