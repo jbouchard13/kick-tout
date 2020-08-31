aa2bcf739476f215707b95bac6d6ec9b4f57c031
Hannah Perreault <hperre@gmail.com>
2020-08-31 15:47:46 -0400

    working on fav bar

--- client/src/components/ShoeCard/ShoeCard.js
+++ client/src/components/ShoeCard/ShoeCard.js
@@ -1,43 +1,54 @@
-import React, { Component } from 'react';
+import React, { Component, useState, useEffect } from 'react';
 import Card from 'react-bootstrap/Card';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHeart } from '@fortawesome/free-solid-svg-icons';
 import { faUser } from '@fortawesome/free-solid-svg-icons';
+import API from '../../utils/API';
 
 export class ShoeCard extends Component {
   render() {
     return (
       <div>
         <Card className="shoeCard">
-          <Card.Img variant='top' src={this.props.photoSrc} />
+          <Card.Img variant="top" src={this.props.photoSrc} />
           <Card.Body>
             <Card.Title>{this.props.name}</Card.Title>
           </Card.Body>
-          <ul className='list-group list-group-flush'>
-            <li className='list-group-item'>
+          <ul className="list-group list-group-flush">
+            <li className="list-group-item">
               Condition: {this.props.shoeCondition}
             </li>
-            <li className='list-group-item'>Size: {this.props.size}</li>
-            <li className='list-group-item'>
+            <li className="list-group-item">Size: {this.props.size}</li>
+            <li className="list-group-item">
               Estimated Value: ${this.props.value}
             </li>
             <li
               href={this.props.postId}
-              className='list-group-item list-group-item-action'
+              className="list-group-item list-group-item-action"
+              //Click event to collect the user ID and post ID of Favorited item and push into the Favorites record
+              onClick={(e) => {
+                console.log(
+                  'User ID: ' +
+                    this.props.userId +
+                    'Post ID: ' +
+                    this.props.postId
+                );
+                API.getProfile().then((posts) =>{console.log(posts)});
+              }}
             >
               <FontAwesomeIcon icon={faHeart} /> Favorite
             </li>
 
             <li
               href={this.props.userId}
-              className='list-group-item list-group-item-action'
+              className="list-group-item list-group-item-action"
             >
               <FontAwesomeIcon icon={faUser} /> Seller Profile
             </li>
           </ul>
           <Card.Body>
-            <Card.Link href='#'>Trade</Card.Link>
-            <Card.Link href='#'>Buy</Card.Link>
+            <Card.Link href="#">Trade</Card.Link>
+            <Card.Link href="#">Buy</Card.Link>
           </Card.Body>
         </Card>
       </div>
