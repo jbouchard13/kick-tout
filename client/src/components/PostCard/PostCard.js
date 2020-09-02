33c2d1af60656e29bedddef880d7ea3ce1d60919
kdawg <mcleod.karla@gmail.com>
2020-09-01 21:22:57 -0400

    edited the form and post shoe css

--- client/src/components/PostCard/PostCard.js
+++ client/src/components/PostCard/PostCard.js
@@ -1,25 +1,30 @@
 import React from 'react';
-import { Card, Button } from 'react-bootstrap';
+import { Card, Button, Col } from 'react-bootstrap';
 
 export default function PostCard(props) {
   return (
-    <Card>
-      <Card.Img variant='top' src={props.photoSrc} />
-      <Card.Body>
-        <Card.Title>{props.name}</Card.Title>
-      </Card.Body>
-      <ul className='list-group list-group-flush'>
-        <li className='list-group-item'>
-          <Button data-postid={props.postId} onClick={props.handleEdit}>
-            Edit
+
+    <Col sm={4}>
+      <Card className="postCard">
+        <Card.Img variant='top' src={props.photoSrc} />
+        <Card.Body>
+          <Card.Title>{props.name}</Card.Title>
+        </Card.Body>
+        <ul className='list-group list-group-flush'>
+          <li className=' list-group-item'>
+            <Button data-postid={props.postId} onClick={props.handleEdit}>
+              Edit
           </Button>
-        </li>
-        <li className='list-group-item'>
-          <Button data-postid={props.postId} onClick={props.handleDelete}>
-            Delete
+          </li>
+          <li className='list-group-item'>
+            <Button data-postid={props.postId} onClick={props.handleDelete}>
+              Delete
           </Button>
-        </li>
-      </ul>
-    </Card>
+          </li>
+        </ul>
+      </Card>
+    </Col>
   );
 }
+
+
--- client/src/components/PostManagement/CreatePostForm.js
+++ client/src/components/PostManagement/CreatePostForm.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Button, Form } from 'react-bootstrap';
+import { Button, Form, Col } from 'react-bootstrap';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import axios from 'axios';
@@ -118,139 +118,140 @@ export default function CreatePostForm() {
 
   return (
     <div>
-      <Form className='form'>
-        <ToastContainer position='top-right' />
-        <Form.Group controlId='formGridPostType'>
-          <Form.Label>Post Type</Form.Label>
-          <Form.Control
-            name='type'
-            value={type}
-            as='select'
-            onChange={(e) => {
-              typeOnChange(e);
+      <Col sm={10}>
+        <Form className='form'>
+          <ToastContainer position='top-right' />
+          <Form.Group controlId='formGridPostType'>
+            <Form.Label>Post Type</Form.Label>
+            <Form.Control
+              name='type'
+              value={type}
+              as='select'
+              onChange={(e) => {
+                typeOnChange(e);
+              }}
+            >
+              <option>Sell</option>
+              <option>Trade</option>
+            </Form.Control>
+          </Form.Group>
+
+          <Form.Group controlId='formGridImage'>
+            <Form.File
+              id='postImageUpload'
+              label='Add Image'
+              onChange={(e) => {
+                imageOnChange(e);
+              }}
+            ></Form.File>
+          </Form.Group>
+
+          <Form.Group controlId='formGridName'>
+            <Form.Label>Item Name</Form.Label>
+            <Form.Control
+              type='text'
+              name='name'
+              value={name}
+              placeholder='Enter Item Name'
+              onChange={(e) => {
+                nameOnChange(e);
+              }}
+            />
+          </Form.Group>
+
+          <Form.Group controlId='formGridSize'>
+            <Form.Label>Size</Form.Label>
+            <Form.Control
+              name='size'
+              value={size}
+              onChange={(e) => {
+                sizeOnChange(e);
+              }}
+              as='select'
+            >
+              <option>4</option>
+              <option>4 1/2</option>
+              <option>5</option>
+              <option>5 1/2</option>
+              <option>6</option>
+              <option>6 1/2</option>
+              <option>7</option>
+              <option>7 1/2</option>
+              <option>8</option>
+              <option>8 1/2</option>
+              <option>9</option>
+              <option>9 1/2</option>
+              <option>10</option>
+              <option>10 1/2</option>
+              <option>11</option>
+              <option>11 1/2</option>
+              <option>12</option>
+              <option>12 1/2</option>
+              <option>13</option>
+              <option>13 1/2</option>
+              <option>14</option>
+              <option>14 1/2</option>
+              <option>15</option>
+              <option>15 1/2</option>
+              <option>16</option>
+            </Form.Control>
+          </Form.Group>
+
+          <Form.Group controlId='formGridBrand'>
+            <Form.Label>Brand</Form.Label>
+            <Form.Control
+              name='brand'
+              value={brand}
+              placeholder='Enter Item Brand'
+              onChange={(e) => {
+                brandOnChange(e);
+              }}
+            />
+          </Form.Group>
+
+          <Form.Group controlId='formGridShoeCondition'>
+            <Form.Label>Item Condition</Form.Label>
+            <Form.Control
+              name='shoeCondition'
+              value={shoeCondition}
+              as='select'
+              onChange={(e) => {
+                conditionOnChange(e);
+              }}
+            >
+              <option>Excellent</option>
+              <option>Very Good</option>
+              <option>Good</option>
+              <option>Fair</option>
+              <option>Poor</option>
+              <option>Very Poor</option>
+            </Form.Control>
+          </Form.Group>
+
+          <Form.Group>
+            <Form.Label>Value $</Form.Label>
+            <Form.Control
+              name='value'
+              value={value}
+              placeholder='Enter Item Value'
+              onChange={(e) => {
+                valueOnChange(e);
+              }}
+            />
+            <Form.Text>Please enter numbers only</Form.Text>
+          </Form.Group>
+
+          <Button
+            type='submit'
+            variant='dark'
+            onClick={(e) => {
+              onFormSubmit(e);
             }}
           >
-            <option>Buy</option>
-            <option>Sell</option>
-            <option>Trade</option>
-          </Form.Control>
-        </Form.Group>
-
-        <Form.Group controlId='formGridImage'>
-          <Form.File
-            id='postImageUpload'
-            label='Add Image'
-            onChange={(e) => {
-              imageOnChange(e);
-            }}
-          ></Form.File>
-        </Form.Group>
-
-        <Form.Group controlId='formGridName'>
-          <Form.Label>Item Name</Form.Label>
-          <Form.Control
-            type='text'
-            name='name'
-            value={name}
-            placeholder='Enter Item Name'
-            onChange={(e) => {
-              nameOnChange(e);
-            }}
-          />
-        </Form.Group>
-
-        <Form.Group controlId='formGridSize'>
-          <Form.Label>Size</Form.Label>
-          <Form.Control
-            name='size'
-            value={size}
-            onChange={(e) => {
-              sizeOnChange(e);
-            }}
-            as='select'
-          >
-            <option>4</option>
-            <option>4 1/2</option>
-            <option>5</option>
-            <option>5 1/2</option>
-            <option>6</option>
-            <option>6 1/2</option>
-            <option>7</option>
-            <option>7 1/2</option>
-            <option>8</option>
-            <option>8 1/2</option>
-            <option>9</option>
-            <option>9 1/2</option>
-            <option>10</option>
-            <option>10 1/2</option>
-            <option>11</option>
-            <option>11 1/2</option>
-            <option>12</option>
-            <option>12 1/2</option>
-            <option>13</option>
-            <option>13 1/2</option>
-            <option>14</option>
-            <option>14 1/2</option>
-            <option>15</option>
-            <option>15 1/2</option>
-            <option>16</option>
-          </Form.Control>
-        </Form.Group>
-
-        <Form.Group controlId='formGridBrand'>
-          <Form.Label>Brand</Form.Label>
-          <Form.Control
-            name='brand'
-            value={brand}
-            placeholder='Enter Item Brand'
-            onChange={(e) => {
-              brandOnChange(e);
-            }}
-          />
-        </Form.Group>
-
-        <Form.Group controlId='formGridShoeCondition'>
-          <Form.Label>Item Condition</Form.Label>
-          <Form.Control
-            name='shoeCondition'
-            value={shoeCondition}
-            as='select'
-            onChange={(e) => {
-              conditionOnChange(e);
-            }}
-          >
-            <option>Very Poor</option>
-            <option>Poor</option>
-            <option>Fair</option>
-            <option>Good</option>
-            <option>Very Good</option>
-            <option>Excellent</option>
-          </Form.Control>
-        </Form.Group>
-
-        <Form.Group>
-          <Form.Label>Value $</Form.Label>
-          <Form.Control
-            name='value'
-            value={value}
-            placeholder='Enter Item Value'
-            onChange={(e) => {
-              valueOnChange(e);
-            }}
-          />
-          <Form.Text>Please enter numbers only</Form.Text>
-        </Form.Group>
-
-        <Button
-          type='submit'
-          variant='dark'
-          onClick={(e) => {
-            onFormSubmit(e);
-          }}
-        >
-          Create new post
+            Create new post
         </Button>
-      </Form>
+        </Form>
+      </Col>
     </div>
   );
 }
