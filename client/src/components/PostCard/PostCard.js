a6eeeba0bc37f3416bd1cee3e431bee57ea4915c
kdawg <mcleod.karla@gmail.com>
2020-09-03 20:24:32 -0400

    edited nav burger color and added green glow hover to nav items

--- client/src/components/PostCard/PostCard.js
+++ client/src/components/PostCard/PostCard.js
@@ -11,13 +11,13 @@ export default function PostCard(props) {
           <Card.Title>{props.name}</Card.Title>
         </Card.Body>
         <ul className='list-group list-group-flush'>
-          <li className=' list-group-item'>
-            <Button data-postid={props.postId} onClick={props.handleEdit}>
+          <li className='list-group-item'>
+            <Button varient='dark' data-postid={props.postId} onClick={props.handleEdit}>
               Edit
           </Button>
           </li>
           <li className='list-group-item'>
-            <Button data-postid={props.postId} onClick={props.handleDelete}>
+            <Button varient='dark' data-postid={props.postId} onClick={props.handleDelete}>
               Delete
           </Button>
           </li>
