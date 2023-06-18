941273f741a4ae906ec8812cf6875fc608a6f520
John Bouchard <jbouchard13@gmail.com>
2023-06-17 22:32:24 -0400

    adding build folder

---
+++ client/build/precache-manifest.88b0217a469c3c94eb3310c47466411c.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b5e2a2cb202afcad946e271c5fb700f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0a274523a6b8be0fda3a",
+    "url": "/static/css/2.36eead04.chunk.css"
+  },
+  {
+    "revision": "699053a1bcfc5ab8c6ac",
+    "url": "/static/css/main.6bdab2b4.chunk.css"
+  },
+  {
+    "revision": "0a274523a6b8be0fda3a",
+    "url": "/static/js/2.df2efdc0.chunk.js"
+  },
+  {
+    "revision": "dc9bb15c0999cb9c24f7535d777b3b4e",
+    "url": "/static/js/2.df2efdc0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "699053a1bcfc5ab8c6ac",
+    "url": "/static/js/main.a14e11ff.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.a14e11ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "05fbb5ed970a924e23b7",
+    "url": "/static/js/runtime-main.81534a39.js"
+  },
+  {
+    "revision": "05acd6d4780dec5556d3f5a69759826c",
+    "url": "/static/media/basementPic.05acd6d4.jpg"
+  },
+  {
+    "revision": "11125c2e54ad969c4f03e2a33cdb901c",
+    "url": "/static/media/brickWall.11125c2e.jpg"
+  },
+  {
+    "revision": "846e0b2840d6f0aac255aa0207437339",
+    "url": "/static/media/feedPic.846e0b28.jpg"
+  },
+  {
+    "revision": "cef757758a31eea01a3936327f85ce2c",
+    "url": "/static/media/goldPlaque.cef75775.png"
+  },
+  {
+    "revision": "e6445c1849c744d54dbe6437f8fef229",
+    "url": "/static/media/greenPin.e6445c18.png"
+  },
+  {
+    "revision": "588c1acd7c21f726a69fb708f6d70c06",
+    "url": "/static/media/pinTack.588c1acd.png"
+  },
+  {
+    "revision": "12849513b0b7a96971eb2fe328ad45c6",
+    "url": "/static/media/rainbowRoom.12849513.jpg"
+  },
+  {
+    "revision": "81e73034da6d5ba1db74ab881a7b53a0",
+    "url": "/static/media/realCork.81e73034.jpg"
+  },
+  {
+    "revision": "21290d253cbbe1c19e1756862f48eee1",
+    "url": "/static/media/silverPlate.21290d25.png"
+  },
+  {
+    "revision": "11fec786b4f2df7908586c4e2fac2db2",
+    "url": "/static/media/spiralGraf.11fec786.jpg"
+  },
+  {
+    "revision": "20091679495a5a916c9c4848481f477b",
+    "url": "/static/media/yellowPaper.20091679.png"
+  }
+]);
