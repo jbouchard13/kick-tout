f55bb5eb00f32973659d07033e5623ca1e33dd1d
John Bouchard <jbouchard13@gmail.com>
2023-06-18 01:31:40 -0400

    fixed env

--- config/config.js
+++ config/config.js
@@ -16,7 +16,7 @@ module.exports = {
     dialect: "mysql",
   },
   production: {
-    use_env_variable: process.env.DB_HOST,
+    use_env_variable: "DB_HOST",
     username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
